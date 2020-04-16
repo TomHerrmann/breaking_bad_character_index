2ca35cc6f862c90ba8c336c16d8a64608316d371
Tom Herrmann <tomherrmanndev@gmail.com>
2020-04-16 16:23:05 -0400

    Initial setup - server  & package.json scripts not setup properly

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) =>
+  res.sendFile(path.resolve(__dirname, '../dist/index.html'))
+);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,44 @@
 const path = require('path');
 
 module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    hotOnly: true,
+    port: 8000,
+    proxy: {
+      '/': {
+        target: 'http://localhost:3000',
+      },
+    },
+  },
   entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };

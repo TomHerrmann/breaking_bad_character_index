b93779557f1c5cfcce7ed4b16bc56a5640c2fc2a
Tom Herrmann <tomherrmanndev@gmail.com>
2020-04-17 09:50:51 -0400

    cleanup

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+
 app.use(express.static(path.resolve(__dirname, '../dist')));
 
-app.get(
-  '/',
-  (req, res) => res.sendFile(path.resolve(__dirname, '../dist/index.html'))
-  // res.send('Hello Express')
+app.get('/', (req, res) =>
+  res.sendFile(path.resolve(__dirname, '../dist/index.html'))
 );
 
 app.use((err, req, res, next) => {

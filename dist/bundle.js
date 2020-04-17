9caa8ed61395c5a1f4d23059c2584e4402c5f743
Tom Herrmann <tomherrmanndev@gmail.com>
2020-04-17 09:45:12 -0400

    SERVER UP AND RUNNING !!

--- dist/bundle.js
+++ dist/bundle.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("// Imports\nvar ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\nexports = ___CSS_LOADER_API_IMPORT___(false);\n// Module\nexports.push([module.i, \"body {\\n  display: flex;\\n}\\n\", \"\"]);\n// Exports\nmodule.exports = exports;\n\n\n//# sourceURL=webpack:///./src/style.css?./node_modules/css-loader/dist/cjs.js");
+eval("// Imports\nvar ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\nexports = ___CSS_LOADER_API_IMPORT___(false);\n// Module\nexports.push([module.i, \"body {\\n  display: flex;\\n  justify-content: center;\\n  background-color: crimson;\\n}\\n\", \"\"]);\n// Exports\nmodule.exports = exports;\n\n\n//# sourceURL=webpack:///./src/style.css?./node_modules/css-loader/dist/cjs.js");
 
 /***/ }),
 
--- server/server.js
+++ server/server.js
@@ -3,8 +3,17 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) =>
-  res.sendFile(path.resolve(__dirname, '../dist/index.html'))
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+app.get(
+  '/',
+  (req, res) => res.sendFile(path.resolve(__dirname, '../dist/index.html'))
+  // res.send('Hello Express')
 );
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

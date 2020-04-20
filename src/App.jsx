478d5e17906fc87517f1c610e65ec513586a26d7
Tom Herrmann <tomherrmanndev@gmail.com>
2020-04-20 17:36:03 -0400

    Setup display chars logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,38 @@ import React, { useState, useEffect } from 'react';
 
 import Search from './components/Search.jsx';
 import CharacterCard from './components/CharacterCard.jsx';
+import formatCharacters from './utils/formatCharacters';
+
+import { defaultCharacters } from './utils/enums';
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [characters, setCharacters] = useState(null);
+  const [displayCharacters, setDisplayCharacters] = useState(defaultCharacters);
+
+  console.log(defaultCharacters);
 
   useEffect(() => {
     const fetchCharacters = async () => {
       const characterPromise = await fetch('/characters');
       const characters = await characterPromise.json();
 
-      setCharacters(characters);
+      setCharacters(formatCharacters(characters));
+      setIsLoading(false);
     };
 
     fetchCharacters();
   }, []);
 
-  console.log('state chars --> ', characters);
   return (
     <div className="app">
       <h1>Breaking Bad Character Index</h1>
       <Search />
-      <CharacterCard />
+      {!isLoading
+        ? displayCharacters.map((displayCharacter) => (
+            <CharacterCard character={characters[displayCharacter]} />
+          ))
+        : null}
     </div>
   );
 };

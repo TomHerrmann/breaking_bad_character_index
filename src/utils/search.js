eb87df72de12fb39dbd12ffea7f6931bf126f54e
Tom Herrmann <tomherrmanndev@gmail.com>
2020-04-20 17:34:51 -0400

    Create search functionality

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,4 +1,13 @@
-const search = (query) => {
-  // if (isOrdered) {
-  // }
+const search = (query, storeArr) => {
+  const results = [];
+
+  for (let i = 0; i < storeArr.length; i++) {
+    if (elem.includes(query)) results.push(elem);
+
+    if (results.length === 6) return results;
+  }
+
+  return results;
 };
+
+export default search;

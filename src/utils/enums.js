9741d8e1f7cbe790ed047fe65f64ea144166c5a7
Tom Herrmann <tomherrmanndev@gmail.com>
2020-04-20 17:35:29 -0400

    Set defualt character enum

---
+++ src/utils/enums.js
@@ -0,0 +1,8 @@
+export const defaultCharacters = [
+  'Walter White Heisenberg',
+  "Jesse Pinkman Jesse Pinkman Cap n' Cook",
+  'Skyler White Sky',
+  'Walter White Jr. Flynn',
+  'Henry Schrader Hank',
+  'Marie Schrader Marie',
+];

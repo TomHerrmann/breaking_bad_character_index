f81de15630cbe29c25571ac30e81704edaf7e66d
Tom Herrmann <tomherrmanndev@gmail.com>
2020-04-21 14:31:52 -0400

    Cleanup

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,8 +1,6 @@
 const search = (query, storeArr) => {
   const results = [];
 
-  console.log(query);
-
   for (let i = 0; i < storeArr.length; i++) {
     if (storeArr[i].includes(query.toLowerCase())) results.push(storeArr[i]);
     if (results.length === 6) return results;

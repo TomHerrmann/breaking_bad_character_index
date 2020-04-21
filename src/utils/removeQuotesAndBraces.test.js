428e5d1f6183c50bbabce3f628c0bc5855dd08f0
Tom Herrmann <tomherrmanndev@gmail.com>
2020-04-21 14:00:06 -0400

    Reformat test files position in dir

--- src/utils/removeQuotesAndBraces.test.js
+++ src/utils/removeQuotesAndBraces.test.js
@@ -1,4 +1,4 @@
-import { removeQuotesAndBraces } from '../../src/utils/removeQuotesAndBraces';
+import { removeQuotesAndBraces } from './removeQuotesAndBraces';
 
 test('should removes quotes and braces from input strings, and add space after commas', () => {
   expect(removeQuotesAndBraces('{"test1,test2"}')).toBe('test1, test2');

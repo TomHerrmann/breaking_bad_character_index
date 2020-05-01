2d9cfdfaeaf652805fc1397dc5b15d74688d7744
Tom Herrmann <tomherrmanndev@gmail.com>
2020-04-30 22:37:49 -0400

    Remove redundant test case

--- webpack.config.js
+++ webpack.config.js
@@ -36,10 +36,6 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         use: ['file-loader'],
       },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader'],
-      },
     ],
   },
   output: {

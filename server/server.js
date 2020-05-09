07b5f37f9d3ac4e3204ec868b6e6ef590081d323
Tom Herrmann <tomherrmanndev@gmail.com>
2020-05-09 12:12:03 -0400

    Added .env variable for port

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const { fetchCharacters } = require('./controllers/characterController');
 

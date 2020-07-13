782010c50641cc4eabba620ff774051281e280a4
Tom Herrmann <tomherrmanndev@gmail.com>
2020-07-13 16:11:24 -0400

    Remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { connect, useStore } from 'react-redux';
+import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actions from './actions/actions';
 
